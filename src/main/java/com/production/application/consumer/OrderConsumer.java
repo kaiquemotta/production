6b639f087870b4150dfbf33b64f93fbc086f69f4
Kaique Motta <kaique.motta@hotmail.com>
2025-02-15 14:10:44 -0300

    feat: reprocessar queee

--- src/main/java/com/production/application/consumer/OrderConsumer.java
+++ src/main/java/com/production/application/consumer/OrderConsumer.java
@@ -12,7 +12,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+
+import com.rabbitmq.client.Channel; // Importação do Channel
+import org.springframework.amqp.core.Message;
 
 @Component
 @RequiredArgsConstructor
@@ -24,21 +29,40 @@ public class OrderConsumer {
     private final List<OrderProcessor> orderProcessors;
 
     @RabbitListener(queues = "queue.orders")
-    public void receivedMessage(String mensagem) {
+    public void receivedMessage(String mensagem, Channel channel, Message message) throws IOException {
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();  // Pegue o deliveryTag da mensagem
         try {
             Order order = objectMapper.readValue(mensagem, Order.class);
             logger.info("📥 Pedido recebido: {}", order);
-
             for (OrderProcessor processor : orderProcessors) {
                 processor.process(order);
             }
-
+            channel.basicAck(deliveryTag, false);
             logger.info("✅ Pedido processado com sucesso: {}", order.orderId());
+
         } catch (JsonProcessingException e) {
             logger.error("🚨 Erro ao desserializar mensagem: {}", mensagem, e);
+            nackMessage(channel, message, deliveryTag);
         } catch (Exception e) {
             logger.error("❌ Erro ao processar pedido: {}", e.getMessage(), e);
+            nackMessage(channel, message, deliveryTag);
             throw e;
         }
     }
+
+    private void nackMessage(Channel channel, Message message, long deliveryTag) {
+                moveMessageToFailureQueue(channel, message, deliveryTag);
+    }
+
+    private void moveMessageToFailureQueue(Channel channel, Message message, long deliveryTag) {
+        try {
+            channel.basicReject(deliveryTag, false); // Ou basicNack com requeue=false
+            logger.error("Mensagem movida para fila de falhas após tentativas excessivas.");
+        } catch (IOException ex) {
+            logger.error("Erro ao mover mensagem para fila de falhas: {}", ex.getMessage(), ex);
+        }
+    }
+
+
 }
+
